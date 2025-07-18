7c9949b99467f8f395412eae88812e0d5d75c2dd
mohammdhusen zhare <mohammadhusenzhare@gmail.com>
2025-07-18 12:56:42 +0530

    majority_element n/3

--- majority_element.cpp
+++ majority_element.cpp
@@ -46,9 +46,65 @@ int majorityElementBrute(vector<int> nums)
     return -1;
 }
 
+// n/3 boore moores voting
+vector<int> majorityElementN3(vector<int> &nums)
+{
+    int count1 = 0, count2 = 0;
+    int ans1 = 0, ans2 = 1;
+    vector<int> result;
+
+    for (int num : nums)
+    {
+        if (num == ans1)
+        {
+            count1++;
+        }
+        else if (num == ans2)
+        {
+            count2++;
+        }
+        else if (count1 == 0)
+        {
+            ans1 = num;
+            count1 = 1;
+        }
+        else if (count2 == 0)
+        {
+            ans2 = num;
+            count2 = 1;
+        }
+        else
+        {
+            count1--;
+            count2--;
+        }
+    }
+
+    count1 = count2 = 0;
+    for (int num : nums)
+    {
+        if (num == ans1)
+            count1++;
+        else if (num == ans2)
+            count2++;
+    }
+
+    int n = nums.size();
+    if (count1 > n / 3)
+        result.push_back(ans1);
+    if (count2 > n / 3)
+        result.push_back(ans2);
+
+    return result;
+}
+
 int main()
 {
-    vector<int> nums = {1, 1, 2, 2, 2, 2};
-    cout << majorityElementBrute(nums) << endl;
+    vector<int> nums = {3, 2, 3};
+    vector<int> result = majorityElementN3(nums);
+    for (int num : result)
+    {
+        cout << num << " ,";
+    }
     return 0;
 }
